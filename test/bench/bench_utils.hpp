da2240581ac2473cc059d6167a6d8398ff7d06fb
atharva.dubey <atharva.dubey@codeplay.com>
2023-04-07 09:43:49 +0100

    removed reference_dft from bench_utils

--- test/bench/bench_utils.hpp
+++ test/bench/bench_utils.hpp
@@ -46,43 +46,4 @@ bool compare_arrays(type* array1, type* array2, size_t num_elements, double absT
   return correct;
 }
 
-/**
- * @brief Multidimensional Refernce DFT implementation
- * 
- * @tparam TypeIn Type of the input
- * @tparam TypeOut Type of the output
- * @param in Pointer of TypeIn pointing to the input
- * @param out Pointer of TypeOut pointing to the output
- * @param length Vector containing the length in every dimension
- * @param offset memory offset for in and out pointers
- */
-template <typename TypeIn, typename TypeOut>
-void reference_forward_dft(TypeIn* in, TypeOut* out, std::vector<int> length, size_t offset = 0) {
-  long double TWOPI = 2.0l * std::atan(1.0l) * 4.0l;
-  std::vector<std::size_t> dims{1, 1, 1};
-  std::copy(length.begin(), length.end(), dims.end());
-
-  for (size_t ox = 0; ox < dims[0]; ox++) {
-    for (size_t oy = 0; oy < dims[1]; oy++) {
-      for (size_t oz = 0; oz < dims[2]; oz++) {
-        std::complex<long double> out_temp = 0;
-        for (size_t ix = 0; ix < dims[0]; ix++) {
-          for (size_t iy = 0; iy < dims[1]; iy++) {
-            for (size_t iz = 0; iz < dims[2]; iz++) {
-              double theta = -1 * TWOPI *
-                             ((ix * ox / static_cast<double>(dims[0])) + (iy * oy / static_cast<double>(dims[1])) +
-                              (iz * oz / static_cast<double>(dims[2])));
-              auto element =
-                  static_cast<std::complex<long double>>(in[offset + ix * dims[1] * dims[2] + iy * dims[2] + iz]);
-              auto multiplier = std::complex<long double>(std::cos(theta), std::sin(theta));
-              out_temp += element * multiplier;
-            }
-          }
-        }
-        out[offset + ox * dims[1] * dims[2] + oy * dims[2] + oz] = out_temp;
-      }
-    }
-  }
-}
-
 #endif //SYCLFFT_BENCH_BENCH_UTILS_HPP
