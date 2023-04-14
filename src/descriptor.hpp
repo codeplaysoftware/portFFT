01fbbcb62ab5db8be5988d686528240b47f6cd86
Tadej Ciglarič <tadej.ciglaric@codeplay.com>
2023-04-14 15:13:08 +0200

    Remove support for input_distance and output_distance (#19)
    * remove support for input_distance and output_distance
    
    * format
    
    * fix doxygen

--- src/descriptor.hpp
+++ src/descriptor.hpp
@@ -256,8 +256,8 @@ class committed_descriptor {
       sycl::local_accessor<Scalar, 1> loc(local_elements, cgh);
       cgh.parallel_for<detail::usm_kernel<Scalar, Domain, dir>>(
           sycl::nd_range<1>{{global_size}, {subgroup_size}}, [=](sycl::nd_item<1> it) {
-            detail::dispatcher<dir>(in_scalar, out_scalar, loc, fft_size, n_transforms, input_distance, output_distance,
-                                    it, twiddles_local, scale_factor);
+            detail::dispatcher<dir>(in_scalar, out_scalar, loc, fft_size, n_transforms, it, twiddles_local,
+                                    scale_factor);
           });
     });
   }
@@ -307,8 +307,8 @@ class committed_descriptor {
       sycl::local_accessor<Scalar, 1> loc(local_elements, cgh);
       cgh.parallel_for<detail::buffer_kernel<Scalar, Domain, dir>>(
           sycl::nd_range<1>{{global_size}, {subgroup_size}}, [=](sycl::nd_item<1> it) {
-            detail::dispatcher<dir>(in_acc.get_pointer(), out_acc.get_pointer(), loc, fft_size, n_transforms,
-                                    input_distance, output_distance, it, twiddles_local, scale_factor);
+            detail::dispatcher<dir>(in_acc.get_pointer(), out_acc.get_pointer(), loc, fft_size, n_transforms, it,
+                                    twiddles_local, scale_factor);
           });
     });
   }
