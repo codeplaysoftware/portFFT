b3cb8db761453f98da6c123402ee610d909ac29b
romain.biessy <romain.biessy@codeplay.com>
2023-05-24 09:14:42 +0100

    Replace tab with space

--- test/bench/utils/reference_dft_set.hpp
+++ test/bench/utils/reference_dft_set.hpp
@@ -36,17 +36,17 @@
 
 // clang-format off
 // Forward, float, out-of-place only:
-// 1. small        complex 1D fits in workitem Cooley-Tukey 	   (batch=8*1024*1024 N=16)
-// 2. medium-small complex 1D fits in subgroup Cooley-Tukey 	   (batch=512*1024    N=256)
-// 3. medium-large complex 1D fits in local memory Cooley-Tukey  (batch=32*1024     N=4*1024)
-// 4. large        complex 1D fits in global memory Cooley-Tukey (batch=2*1024      N=64*1024)
-// 5. large        complex 1D fits in global memory Bluestein    (batch=2*1024      N=64*1024+1)
-// 6. large        complex 2D fits in global memory              (batch=8           N=4096x4096)
-// 7. small        real    1D fits in workitem Cooley-Tukey 	   (batch=8*1024*1024 N=32)
-// 8. medium-small real    1D fits in subgroup Cooley-Tukey 	   (batch=512*1024    N=512)
-// 9. medium-large real    1D fits in local memory Cooley-Tukey  (batch=32*1024     N=8*1024)
-// 10. large       real    1D fits in global memory Cooley-Tukey (batch=2*1024      N=128*1024)
-// 11. small       real    3D                                    (batch=1024        N=64x64x64)
+// 1. small        complex 1D fits in workitem Cooley-Tukey        (batch=8*1024*1024 N=16)
+// 2. medium-small complex 1D fits in subgroup Cooley-Tukey        (batch=512*1024    N=256)
+// 3. medium-large complex 1D fits in local memory Cooley-Tukey    (batch=32*1024     N=4*1024)
+// 4. large        complex 1D fits in global memory Cooley-Tukey   (batch=2*1024      N=64*1024)
+// 5. large        complex 1D fits in global memory Bluestein      (batch=2*1024      N=64*1024+1)
+// 6. large        complex 2D fits in global memory                (batch=8           N=4096x4096)
+// 7. small        real    1D fits in workitem Cooley-Tukey        (batch=8*1024*1024 N=32)
+// 8. medium-small real    1D fits in subgroup Cooley-Tukey        (batch=512*1024    N=512)
+// 9. medium-large real    1D fits in local memory Cooley-Tukey    (batch=32*1024     N=8*1024)
+// 10. large       real    1D fits in global memory Cooley-Tukey   (batch=2*1024      N=128*1024)
+// 11. small       real    3D                                      (batch=1024        N=64x64x64)
 //
 // Configurations must match with the ones in test/bench/sycl-fft/launch_bench.hpp
 
