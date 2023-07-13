b34115eed92725cbbd45a5a5b8636dc86cb0f59d
Hugh Bird <hugh.bird@codeplay.com>
2023-07-13 09:54:25 +0100

    De-duplicate supported size warnings; Comment that factorization is duplicated

--- src/descriptor.hpp
+++ src/descriptor.hpp
@@ -163,11 +163,11 @@ class committed_descriptor {
   detail::level prepare_implementation(std::vector<sycl::kernel_id>& ids) {
     factors.clear();
     std::size_t fft_size = params.lengths[0];
+    if (!detail::cooley_tukey_size_list_t::has_size(fft_size)) {
+      throw std::runtime_error("FFT size " + std::to_string(fft_size) + " is not supported!");
+    }
     if (detail::fits_in_wi<Scalar>(fft_size)) {
       get_ids<detail::workitem_kernel, SubgroupSize>(ids);
-      if(!detail::cooley_tukey_size_list_t::has_size(fft_size)){
-        throw std::runtime_error("FFT size " + std::to_string(fft_size) + " is not supported!");
-      }
       return detail::level::WORKITEM;
     }
     int factor_sg = detail::factorize_sg(static_cast<int>(fft_size), SubgroupSize);
@@ -178,9 +178,6 @@ class committed_descriptor {
       factors.push_back(factor_wi);
       factors.push_back(factor_sg);
       get_ids<detail::subgroup_kernel, SubgroupSize>(ids);
-      if(!detail::cooley_tukey_size_list_t::has_size(fft_size)){
-        throw std::runtime_error("FFT size " + std::to_string(fft_size) + " is not supported!");
-      }
       return detail::level::SUBGROUP;
     }
     std::size_t N = detail::factorize(fft_size);
@@ -194,10 +191,9 @@ class committed_descriptor {
       factors.push_back(factor_sg_N);
       factors.push_back(factor_wi_M);
       factors.push_back(factor_sg_M);
+      // This factorization of N and M is duplicated in the dispatch logic on the device.
+      // The CT and spec constant factors should match.
       get_ids<detail::workgroup_kernel, SubgroupSize>(ids);
-      if(!detail::cooley_tukey_size_list_t::has_size(fft_size)){
-        throw std::runtime_error("FFT size " + std::to_string(fft_size) + " is not supported!");
-      }
       return detail::level::WORKGROUP;
     }
     // TODO global
