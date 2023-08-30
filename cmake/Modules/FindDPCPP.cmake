55cc425e87df1b3da87961cfe11ee9a7a50dfbea
Hugh Bird <hugh.bird@codeplay.com>
2023-08-30 12:17:14 +0100

    Add cmake flags for easily enabling clang's optimization remarks
    * Adds PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX
    * Default value is "" - no remarks!
    * When set, the clang optimization remarks are generated for the
    selected optimization pass(es). A .opt.yaml file is also saved.
    * Eg: set to .* will give remarks for all optimizations. Set to
    loop-unroll will remark for loop unrolling. See clang docs for
    details on optimization passes.

--- cmake/Modules/FindDPCPP.cmake
+++ cmake/Modules/FindDPCPP.cmake
@@ -45,8 +45,13 @@ if(DPCPP_FOUND AND NOT TARGET DPCPP::DPCPP)
   set(CMAKE_CXX_STANDARD 17)
   add_library(DPCPP::DPCPP INTERFACE IMPORTED)
   set_target_properties(DPCPP::DPCPP PROPERTIES
-    INTERFACE_COMPILE_OPTIONS "-fsycl;-fsycl-targets=${PORTFFT_DEVICE_TRIPLE};-fsycl-unnamed-lambda"
-    INTERFACE_LINK_OPTIONS "-fsycl;-fsycl-targets=${PORTFFT_DEVICE_TRIPLE};-fsycl-unnamed-lambda"
+    set(COMPILE_FLAGS "-fsycl;-fsycl-targets=${PORTFFT_DEVICE_TRIPLE};-fsycl-unnamed-lambda")
+    if(NOT PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX STREQUAL "")
+      STRING(REPLACE "<regex>" "${PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX}" REMARK_ADDITIONAL_FLAGS "-fsave-optimization-record;-Rpass-missed=<regex>;-Rpass=<regex>;-Rpass-analysis=<regex>;")
+      LIST(PREPEND COMPILE_FLAGS ${REMARK_ADDITIONAL_FLAGS})
+    endif()
+    INTERFACE_COMPILE_OPTIONS ${COMPILE_FLAGS}
+    INTERFACE_LINK_OPTIONS ${COMPILE_FLAGS}
     INTERFACE_LINK_LIBRARIES ${DPCPP_LIB}
     INTERFACE_INCLUDE_DIRECTORIES "${DPCPP_BIN_DIR}/../include/sycl;${DPCPP_BIN_DIR}/../include")
 endif()
@@ -60,5 +65,5 @@ function(add_sycl_to_target)
     "${multi_value_args}"
     ${ARGN}
   )
-  target_link_libraries(${ARG_TARGET} PUBLIC DPCPP::DPCPP)                        
+  target_link_libraries(${ARG_TARGET} PUBLIC DPCPP::DPCPP)
 endfunction()
