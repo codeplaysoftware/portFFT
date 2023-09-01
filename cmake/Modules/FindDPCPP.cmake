8e6b0c47e8ba642118ee9f54dd317d103fe46bfb
Hugh Bird <hugh.bird@codeplay.com>
2023-09-01 11:53:43 +0100

    Fix bad implementation for DPCPP nightly

--- cmake/Modules/FindDPCPP.cmake
+++ cmake/Modules/FindDPCPP.cmake
@@ -44,12 +44,8 @@ mark_as_advanced(DPCPP_FOUND DPCPP_LIB)
 if(DPCPP_FOUND AND NOT TARGET DPCPP::DPCPP)
   set(CMAKE_CXX_STANDARD 17)
   add_library(DPCPP::DPCPP INTERFACE IMPORTED)
+  set(COMPILE_FLAGS "-fsycl;-fsycl-targets=${PORTFFT_DEVICE_TRIPLE};-fsycl-unnamed-lambda")
   set_target_properties(DPCPP::DPCPP PROPERTIES
-    set(COMPILE_FLAGS "-fsycl;-fsycl-targets=${PORTFFT_DEVICE_TRIPLE};-fsycl-unnamed-lambda")
-    if(NOT PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX STREQUAL "")
-      STRING(REPLACE "<regex>" "${PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX}" REMARK_ADDITIONAL_FLAGS "-fsave-optimization-record;-Rpass-missed=<regex>;-Rpass=<regex>;-Rpass-analysis=<regex>;")
-      LIST(PREPEND COMPILE_FLAGS ${REMARK_ADDITIONAL_FLAGS})
-    endif()
     INTERFACE_COMPILE_OPTIONS ${COMPILE_FLAGS}
     INTERFACE_LINK_OPTIONS ${COMPILE_FLAGS}
     INTERFACE_LINK_LIBRARIES ${DPCPP_LIB}
@@ -65,5 +61,13 @@ function(add_sycl_to_target)
     "${multi_value_args}"
     ${ARGN}
   )
+  set(ADDITIONAL_FLAGS "")
+  if(NOT PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX STREQUAL "")
+    STRING(REPLACE "<regex>" "${PORTFFT_CLANG_OPTIMIZATION_REMARKS_REGEX}" REMARK_ADDITIONAL_FLAGS "-fsave-optimization-record;-Rpass-missed=<regex>;-Rpass=<regex>;-Rpass-analysis=<regex>;")
+    LIST(PREPEND ADDITIONAL_FLAGS ${REMARK_ADDITIONAL_FLAGS})
+    message(STATUS Flags... ${ADDITIONAL_FLAGS})
+  endif()
   target_link_libraries(${ARG_TARGET} PUBLIC DPCPP::DPCPP)
+  target_compile_options(${ARG_TARGET} PUBLIC ${ADDITIONAL_FLAGS})
+  target_link_options(${ARG_TARGET} PUBLIC ${ADDITIONAL_FLAGS})
 endfunction()
