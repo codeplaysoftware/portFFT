399aa70246cd4ba5725ce17f1a62603da0748b3d
atharva.dubey <atharva.dubey@codeplay.com>
2023-09-05 09:23:37 +0100

    fix warnings

--- src/dispatcher/workgroup_dispatcher.hpp
+++ src/dispatcher/workgroup_dispatcher.hpp
@@ -257,7 +257,7 @@ struct committed_descriptor<Scalar, Domain>::calculate_twiddles_struct::inner<de
     Scalar* global_pointer = res + 2 * (n + m);
     // Copying from pinned memory to device might be faster than from regular allocation
     Scalar* temp_host = sycl::malloc_host<Scalar>(2 * fft_size, desc.queue);
-    Scalar* scratch_memory = new Scalar[2 * factor_sg_m * factor_wi_m];
+    Scalar* scratch_memory = new Scalar[static_cast<std::size_t>(2 * factor_sg_m * factor_wi_m)];
 
     for (std::size_t i = 0; i < n; i++) {
       for (std::size_t j = 0; j < m; j++) {
@@ -279,7 +279,7 @@ struct committed_descriptor<Scalar, Domain>::calculate_twiddles_struct::inner<de
               temp_host[row_offset + static_cast<std::size_t>(2 * k * factor_wi_m + 2 * j + 1)];
         }
       }
-      for (std::size_t j = 0; j < static_cast<std::size_t>(2 * factor_sg_m * factor_wi_m); j++) {
+      for (std::size_t j = 0; j < 2 * m; j++) {
         temp_host[row_offset + j] = scratch_memory[j];
       }
     }
