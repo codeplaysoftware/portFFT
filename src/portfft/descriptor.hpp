e0f08bef7f1c6417d378e400f5d9be5324300b18
atharva.dubey <atharva.dubey@codeplay.com>
2023-11-22 17:02:55 +0000

    fix build with clang and remove pragma to ignore -Wintel-compat

--- src/portfft/descriptor.hpp
+++ src/portfft/descriptor.hpp
@@ -179,18 +179,18 @@ class committed_descriptor {
   template <typename Scalar1, domain Domain1, direction Dir, detail::layout LayoutIn, detail::layout LayoutOut,
             Idx SubgroupSize, typename TIn>
   friend std::vector<sycl::event> detail::compute_level(
-      const typename committed_descriptor<Scalar1, Domain1>::kernel_data_struct& kd_struct, TIn input, Scalar* output,
-      const Scalar* twiddles_ptr, const IdxGlobal* factors_triple, Scalar scale_factor,
+      const typename committed_descriptor<Scalar1, Domain1>::kernel_data_struct& kd_struct, TIn input, Scalar1* output,
+      const Scalar1* twiddles_ptr, const IdxGlobal* factors_triple, Scalar1 scale_factor,
       IdxGlobal intermediate_twiddle_offset, IdxGlobal subimpl_twiddle_offset, IdxGlobal input_global_offset,
       IdxGlobal committed_size, Idx num_batches_in_l2, IdxGlobal n_transforms, IdxGlobal batch_start, Idx factor_id,
       Idx total_factors, const std::vector<sycl::event>& dependencies, sycl::queue& queue);
 
   template <typename Scalar1, domain Domain1, typename TOut>
   friend sycl::event detail::transpose_level(
-      const typename committed_descriptor<Scalar1, Domain1>::kernel_data_struct& kd_struct, const Scalar* input,
+      const typename committed_descriptor<Scalar1, Domain1>::kernel_data_struct& kd_struct, const Scalar1* input,
       TOut output, const IdxGlobal* factors_triple, IdxGlobal committed_size, Idx num_batches_in_l2,
       IdxGlobal n_transforms, IdxGlobal batch_start, Idx factor_num, Idx total_factors, IdxGlobal output_offset,
-      sycl::queue& queue, std::shared_ptr<Scalar>& ptr1, std::shared_ptr<Scalar>& ptr2,
+      sycl::queue& queue, std::shared_ptr<Scalar1>& ptr1, std::shared_ptr<Scalar1>& ptr2,
       const std::vector<sycl::event>& events);
 
   descriptor<Scalar, Domain> params;
