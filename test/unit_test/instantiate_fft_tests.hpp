7c1d77a7199331b7e9053c2f0e0832c7198223fd
Hugh Bird <hugh.bird@codeplay.com>
2024-01-09 16:50:05 +0000

    Fix hang with subgroup implementation on Nvidia
    * Fix hang
    * Add test

--- test/unit_test/instantiate_fft_tests.hpp
+++ test/unit_test/instantiate_fft_tests.hpp
@@ -62,6 +62,8 @@ auto all_valid_multi_dim_placement_layouts = ::testing::ValuesIn(valid_multi_dim
 
 auto ip_packed_layout = ::testing::Values(
     test_placement_layouts_params{placement::IN_PLACE, detail::layout::PACKED, detail::layout::PACKED});
+auto ip_batch_interleaved_layout = ::testing::Values(
+    test_placement_layouts_params{placement::IN_PLACE, detail::layout::BATCH_INTERLEAVED, detail::layout::BATCH_INTERLEAVED});
 
 auto oop_packed_layout = ::testing::Values(
     test_placement_layouts_params{placement::OUT_OF_PLACE, detail::layout::PACKED, detail::layout::PACKED});
@@ -98,6 +100,12 @@ INSTANTIATE_TEST_SUITE_P(SubgroupTest, FFTTest,
                          ::testing::ConvertGenerator<basic_param_tuple>(::testing::Combine(
                              all_valid_placement_layouts, fwd_only, complex_storages, ::testing::Values(1, 3, 555),
                              ::testing::Values(sizes_t{64}, sizes_t{96}, sizes_t{128}))),
+                         test_params_print());
+// sizes that use subgroup implementation
+INSTANTIATE_TEST_SUITE_P(SubgroupRegressionTest, FFTTest,
+                         ::testing::ConvertGenerator<basic_param_tuple>(
+                             ::testing::Combine(ip_batch_interleaved_layout, fwd_only, interleaved_storage,
+                                                ::testing::Values(44, 100), ::testing::Values(sizes_t{100}))),
                          test_params_print());
 // sizes that might use subgroup or workgroup implementation depending on device
 // and configurations
