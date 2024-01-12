785e0df00e92de56a165c03b953109fb386ed4be
Hugh Bird <hugh.bird@codeplay.com>
2024-01-12 15:16:24 +0000

    Use only 1 68640 combination

--- test/unit_test/instantiate_fft_tests.hpp
+++ test/unit_test/instantiate_fft_tests.hpp
@@ -138,13 +138,13 @@ INSTANTIATE_TEST_SUITE_P(WorkgroupOrGlobal, FFTTest,
 INSTANTIATE_TEST_SUITE_P(GlobalTest, FFTTest,
                          ::testing::ConvertGenerator<basic_param_tuple>(::testing::Combine(
                              all_valid_global_placement_layouts, fwd_only, interleaved_storage, ::testing::Values(1, 3),
-                             ::testing::Values(sizes_t{32768}, sizes_t{68640}, sizes_t{65536}, sizes_t{131072}))),
+                             ::testing::Values(sizes_t{32768}, sizes_t{65536}, sizes_t{131072}))),
                          test_params_print());
 
 INSTANTIATE_TEST_SUITE_P(WorkgroupOrGlobalRegressionTest, FFTTest,
                          ::testing::ConvertGenerator<basic_param_tuple>(
                              ::testing::Combine(ip_packed_layout, fwd_only, interleaved_storage, ::testing::Values(3),
-                                                ::testing::Values(sizes_t{9800}, sizes_t{15360}))),
+                                                ::testing::Values(sizes_t{9800}, sizes_t{15360}, sizes_t{68640}))),
                          test_params_print());
 
 // Backward FFT test suite
